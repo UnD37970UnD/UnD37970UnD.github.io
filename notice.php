71c885faf96b13d5839136dd7c74cc6ec14b5cf7
Iustin Ivan <iustinivan2002@google.com>
2021-07-11 12:25:58 +0300

    #rd commit

---
+++ notice.php
@@ -0,0 +1,444 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hacked By FRK48 - Indonesian Freedom Security</title>
+
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Audiowide');
+        @import url('https://fonts.googleapis.com/css?family=Bungee');
+        @import url('https://fonts.googleapis.com/css?family=Orbitron');
+        * {
+            margin: 0 auto;
+            padding: 0;
+        }
+
+        h1,
+        h2,
+        h3 {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            text-align: center;
+            background-color: black;
+            background-size: auto;
+        }
+
+
+
+        span {
+            display: inline-block;
+            position: relative;
+            user-select: none;
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            cursor: default;
+            text-align: center;
+
+        }
+
+        h1.dead {
+            color: #33FF00;
+            font-family: 'Audiowide', cursive;
+            text-transform: uppercase;
+            font-size: 35px;
+            font-weight: 200;
+            text-align: center;
+        }
+
+        h1.dead {
+            text-shadow: 0px 0px 10px red,
+            0px 0px 20px red,
+            0px 0px 30px red,
+            0px 0px 40px red,
+            0px 0px 50px red,
+            0px 0px 60px red;
+            animation: pagla 0.1s infinite;
+        }
+
+
+        @keyframes pagla {
+            0% {
+                text-shadow: 0px 0px 0px;
+                opacity: 0;
+            }
+            50% {
+                text-shadow: 0px 0px 10px red,
+                0px 0px 20px red,
+                0px 0px 30px red,
+                0px 0px 40px red,
+                0px 0px 50px red,
+                0px 0px 60px red;
+                opacity: 1;
+            }
+            100% {
+                text-shadow: 0px 0px 0px;
+                opacity: 0;
+            }
+        }
+
+        h1.dead2 {
+            color: #fff;
+            font-size: 30px;
+            font-family: 'Bungee', cursive;
+        }
+
+        h1.dead2 {
+            text-shadow: 0px 0px 10px #33FF00,
+            0px 0px 20px #33FF00,
+            0px 0px 30px #33FF00,
+            0px 0px 40px #33FF00,
+            0px 0px 50px #33FF00,
+            0px 0px 60px #33FF00;
+        }
+
+        span.pagla-1 {
+            animation: pagla-1 6s infinite;
+        }
+
+        span.pagla-2 {
+            animation: pagla-2 3s infinite;
+        }
+
+        span.pagla-3 {
+            animation: pagla-3 5s infinite;
+        }
+
+        @keyframes pagla-1 {
+            0% {
+                opacity: 1;
+            }
+            52% {
+                opacity: 1;
+            }
+            55% {
+                opacity: 0;
+            }
+            56% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @keyframes pagla-2 {
+            0% {
+                opacity: 1;
+            }
+            62% {
+                opacity: 1;
+            }
+            65% {
+                opacity: 0;
+            }
+            66% {
+                opacity: 1;
+            }
+            69% {
+                opacity: 0;
+            }
+            70% {
+                opacity: 1;
+            }
+            73% {
+                opacity: 0;
+            }
+            74% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+
+        @keyframes pagla-3 {
+            0% {
+                opacity: 1;
+            }
+            82% {
+                opacity: 1;
+            }
+            84% {
+                opacity: 0;
+            }
+            85% {
+                opacity: 1;
+            }
+            87% {
+                opacity: 0;
+            }
+            88% {
+                opacity: 1;
+            }
+            90% {
+                opacity: 0;
+            }
+            91% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+
+        
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <<br>
+<img src="http://pentesterbiasa.com/ifs.png" width='220' height='220'>
+<br>
+<br>
+<br>
+<font size="5px" color="white"><b>Peace World :) </b></font>
+<br>
+<br>
+<font size="4px" color="grey">I just want to Say </font>
+<br>
+<font size="4px" color="grey">" That I just want a world of peace without </font><font size="4px" color="red">Racism</font><font size="4px" color="grey">,</font> <font size="4px" color="red">Discrimination</font><font size="4px" color="grey">,</font> <font size="4px" color="grey">and</font> <font size="4px" color="red">Genocide</font> <font size="4px" color="grey">"</font> 
+<br> 
+<img src="http://kodbul.org/img/konuayirac/siyahyuruyen.gif" border="0">
+<br>
+<br>
+<font size="5px" color="red"><b>Hacked By FRK48</b></font>
+<br>
+<br>
+<a href="https://www.facebook.com/IndoFreeSec/?ref=bookmarks"target="_blank">
+<font size="4px" color="red">We Are Indonesian Freedom Security</a></font>
+<br><font size='3px' color='white'> AlHikam0x - </font><font size='3px' color='red'>FRK48</font><font size='3px' color='white'> - 17_PersonNotFound - Mujahidin303 - grav3 - ./Mr.HanzID - Mr.Trouble5hooting - LolzSec - AL-VRI - Kaizen aka Xenzia Worm - UseYourBrain - Kuuhaku - LazyUser_ - @NubzSec - Mr.X98 - ./SahaainG./ - Mr.Vangke404 </font>
+<br>
+<br>
+<font size='4px' color='red'><b>
+{ Shootz }</b> </font>
+<br>
+<marquee scrollamount="6" direction="left" width="80%"><font size='3px' color='white'>[ ./Cyber00t - Walkers404 - Resis-07 - Gend3ruw0 - ACE666X - Con7ext - E7B_404 - Fazlast - INyourDre4m - LoliCyndrome - FRU_403 - GalehDotId - ZeroBy7es - Mr.Luciferz - Oli404 - L3m0t N3t - Azzatssins - XnonGremX - 5iNON!MOU23 - CCOCOT - Ashura - Monkey B Luffy - Jiilan404 - B0c4H_Id30T - ./N4sKun ] [ Tatsumi Crew - Frbdn403 - Ksk_WTG - XaiSyndicate - PhantomGhost - BerdendangC0de - BAHARI TROUBLE MAKER - 1ntr0ver7 Tersakiti - Garooda Security Squad - Hunter Security -  HentaiC0de - Family Attack Cyber - N45HT - IndoXploit - Indonesia Defacer Tersakiti Team ]</marquee></font></div>
+
+<br>
+<br>
+<font size='4px' color='red'> © Indonesian Hacker Rulez </font>
+<br>
+<br>
+<br>
+ <iframe width="0" height="0" src="https://www.youtube.com/embed/JfsG-i587lU?rel=0&autoplay=1" frameborder="0" allowfullscreen>
+</iframe>
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </span>
+
+
+
+
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+
+
+    <script>
+        (function() {
+
+
+
+            var v = 10.0;
+            var m = 10;
+            var n = 250;
+            var f = 40;
+            var r = 40;
+
+
+
+            var con, dir, star;
+            var d = document;
+
+
+            var cols = ['#9db4ff', '#a2b9ff', '#a7bcff', '#aabfff', '#afc3ff', '#baccff', '#c0d1ff', '#fff',
+                '#cad8ff', '#e4e8ff', '#edeeff', '#fbf8ff', '#fff9f9', '#fff5ec', '#fff4e8', '#fff',
+                '#fff1df', '#ffebd1', '#ffd7ae', '#ffc690', '#ffbe7f', '#ffbb7b', '#ffbb7b'
+            ];
+
+            var depth = 2000;
+            var deg = 180 / Math.PI;
+            var rad = Math.PI / 180;
+
+            var mls = 1000 / 60;
+            var ini = performance.now();
+            var strs = [],
+                starCoords = [];
+            var xyo = -(Math.PI * 2);
+
+            con = d.createElement('div');
+            con.setAttribute('style', 'display:block;' +
+                'position:fixed;' +
+                'height:100%;width:100%;' +
+                'margin:auto;' +
+                'top:0;left:0;right:0;bottom:0;' +
+                'overflow:hidden;' +
+                'visibility:hidden;');
+            d.body.appendChild(con);
+
+            for (var i = 0; i < n; i++) {
+                var col = cols[Math.random() * cols.length | 0];
+                strs[i] = d.createElement('div');
+                strs[i].setAttribute('style', 'display:block;' +
+                    'position:absolute;' +
+                    'height:200px;' +
+                    'width:200px;' +
+                    'margin-top:-100px;' +
+                    'margin-left:-100px;' +
+                    'background-color:' + col + ';' +
+                    'border-radius: 50%;' +
+                    'visibility:visible;');
+                con.appendChild(strs[i]);
+            }
+
+            dir = d.createElement('div');
+            dir.setAttribute('style', 'display:block;' +
+                'position:fixed;' +
+                'bottom:0;right:0;' +
+                'margin:3px;' +
+                'height:auto;width:auto;' +
+                'border:1px solid #fff;' +
+                'padding:5px;letter-spacing:1px;' +
+                'font:11px Verdana,Courier new;' +
+                'text-align:center;color:#fff;' +
+                'cursor:n-resize;opacity:0.1;' +
+                'visibility:visible;');
+            con.appendChild(dir);
+            dir.innerHTML += '';
+
+            function initCoords() {
+                for (var i = 0; i < n; i++) {
+                    star = {
+                        x: coords(1),
+                        y: coords(2),
+                        z: coords(),
+                        o: 0
+                    };
+                    starCoords.push(star);
+                    star.o = 1 - (0.8 * star.z / depth);
+                }
+            }
+
+            function coords(a) {
+                var rnd = Math.floor(Math.random() * r);
+                var sel = (rnd == 0) ? 2.5 : 1.1;
+                var xyz;
+                if (a == 1) {
+                    xyz = f + Math.random() * f / sel *
+                        Math.sin(Math.random() * deg);
+                } else if (a == 2) {
+                    xyz = f + Math.random() * f / sel *
+                        Math.cos(Math.random() * deg);
+                } else {
+                    xyz = Math.random() * depth;
+                }
+                return xyz;
+            }
+
+            function animateCoords(i) {
+                var star = starCoords[i];
+                var x, y, s;
+                star.z -= v;
+                if (v >= 0) {
+                    if (star.z < 0) {
+                        star.z = depth;
+                        star.x = coords(1);
+                        star.y = coords(2);
+                        star.o = 0.05;
+                    }
+                    if (star.o < 1) {
+                        star.o += (v / 900);
+                    }
+                } else if (v < 0) {
+                    if (star.z > depth) {
+                        star.z = -1;
+                        star.x = coords(1);
+                        star.y = coords(2);
+                        star.o = 1.0;
+                    }
+                    if (star.z > 1000) {
+                        star.o += (v / 1100);
+                    }
+                }
+                x = xyo + (star.x - f) * (depth / star.z) + deg;
+                y = xyo + (star.y - f) * (depth / star.z) + deg;
+                s = (1 * (depth / star.z)) / 130;
+                strs[i].style.transform = 'translate3d(' + x + 'vw, ' + y + 'vh, 0) scale3d(' + s + ', ' + s + ', 1)';
+                strs[i].style.opacity = star.o;
+                strs[i].style.transformOrigin = '50 50';
+                strs[i].style.zIndex = Math.round(-star.z);
+            }
+
+            function mouseCon(e) {
+                var tmp = (e.detail) ? -e.detail / 3 : e.wheelDelta / 120;
+                var inc = (tmp >= 0) ? 0.5 : -0.5;
+                v += inc;
+                if (v < -m) {
+                    v += 0.5;
+                }
+                if (v > m) {
+                    v -= 0.5;
+                }
+                e.preventDefault();
+            }
+
+            function draw() {
+                var now = performance.now();
+                if ((now - ini) > mls) {
+                    for (var i = 0; i < n; i++) {
+                        animateCoords(i);
+                    }
+                    ini = performance.now();
+                }
+                window.requestAnimationFrame(draw);
+            }
+
+            function init() {
+                initCoords();
+                draw();
+            }
+
+            dir.addEventListener("mouseout", function() {
+                dir.style.opacity = 0.1;
+            }, false);
+            dir.addEventListener("mouseover", function() {
+                dir.style.opacity = 0.5;
+            }, false);
+            dir.addEventListener('mousewheel', mouseCon, false);
+            dir.addEventListener('DOMMouseScroll', mouseCon, false);
+            window.addEventListener("load", init, false);
+        })();
+    </script>
+
+
+
+
+
+</body>
+
+</html>
